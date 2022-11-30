17c4b9347950edd884b461de88b9205114b03b09
tadzyla <tadas.zvinklys@gmail.com>
2022-11-30 14:13:56 +0000

    change function to reload after wrong input

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function computerPlay() {
     
     let computerHandPick = Math.floor(Math.random() * 3);
 
-    if ( computerHandPick === 0 ) {
+    if (computerHandPick === 0) {
         computerHandPick = "rock";
     } 
-    else if ( computerHandPick === 1 ) {
+    else if (computerHandPick === 1) {
         computerHandPick = "paper";
     } 
     else {
@@ -21,19 +21,19 @@ function playRound(playerSelection, computerSelection) {
 
     let result = "You lose! Scissors beats Paper";
 
-    if ( computerSelection === playerSelection ) {
+    if (computerSelection === playerSelection) {
         result = "Try again";
     }
-    else if ( computerSelection === "rock" && playerSelection === "paper" ) {
+    else if (computerSelection === "rock" && playerSelection === "paper") {
         result = "You win! Paper beats Rock";
     }
-    else if ( computerSelection === "rock" && playerSelection === "scissors" ) {
+    else if (computerSelection === "rock" && playerSelection === "scissors") {
         result = "You lose! Rock beats Scissors";
     }
-    else if ( computerSelection === "paper" && playerSelection === "rock" ) {
+    else if (computerSelection === "paper" && playerSelection === "rock") {
         result = "You lose! Paper beats Rock";
     }
-    else if ( computerSelection === "paper" && playerSelection === "scissors" ){
+    else if (computerSelection === "paper" && playerSelection === "scissors") {
         result = "You win! Scissors beats Paper";
     }
     else {
@@ -45,14 +45,20 @@ function playRound(playerSelection, computerSelection) {
 
 
 function game() {
-    
-    const playerSelection = window.prompt("Type your hand").toLowerCase();
 
-    for( let i = 0; i < 5; i++ ) {
-        console.log(`Player picked: ${playerSelection}`);
-        let computerSelection = computerPlay();
-        console.log(`Computer picked: ${computerSelection}`);
-        console.log(playRound(playerSelection, computerSelection));
+    const playerSelection = window.prompt("Type your hand here ('paper', 'scissors' or 'rock')", "paper").toLowerCase();
+
+    for (let i = 0; i < 5; i++) {
+        if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+            console.log(`Player picked: ${playerSelection}`);
+            let computerSelection = computerPlay();
+            console.log(`Computer picked: ${computerSelection}`);
+            console.log(playRound(playerSelection, computerSelection));
+        } else {
+            alert("Choose the right hand");
+            location.reload();
+            break;
+        }
     }
 }
 
