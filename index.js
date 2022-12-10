95435ad7d08eecf57ed78c50bddd788383d0087f
tadzyla <tadas.zvinklys@gmail.com>
2022-12-10 11:15:31 +0000

    updating logic for winner

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const userHand = document.getElementById("user_hand")
 const elfHand = document.getElementById("elf_hand")
 const handButtons = document.querySelectorAll(".hand_buttons")
 const playerMessage = document.getElementById("user_message")
+const myScore = document.getElementById("my_score")
+const elfScore = document.getElementById("elf_score")
 
 handButtons.forEach(button => {
     button.addEventListener("click", game)
@@ -12,10 +14,15 @@ function game(playerHand) {
     const computerHand = computerPlay()
     elfHand.src = `photos/${computerHand}.png`
     userHand.src = `photos/${playerHand.target.value}.png`
-    playerMessage.textContent = getMessage(computerHand, playerHand.target.value)
+    playerMessage.textContent = getMessageAndUpdateScores(computerHand, playerHand.target.value)
+    if (playerMessage.textContent === "Elf scored 5 points, you lost!" || playerMessage.textContent === "You scored 5 points, you are winner!") {
+        setTimeout(() => {
+            location.reload()
+        }, 1000, );
+    }
 }
 
-function getMessage(computerHand, playerHand) {
+function getMessageAndUpdateScores(computerHand, playerHand) {
 
     if (playerHand === computerHand){ 
         return "It's a draw!" 
@@ -23,9 +30,20 @@ function getMessage(computerHand, playerHand) {
     if ((playerHand === "Rock" && computerHand === "Paper") || 
         (playerHand === "Paper" && computerHand === "Scissors") || 
         (playerHand === "Scissors" && computerHand === "Rock")){
-        return "Elf won!"
+        elfScore.textContent = Number(elfScore.textContent) + 1
+        if (elfScore.textContent < 5) {
+            return "Elf won!"
+        } else {
+            return "Elf scored 5 points, you lost!"
+        }
     }
-        return "You won!"
+        myScore.textContent = Number(myScore.textContent) + 1
+
+        if (myScore.textContent < 5) {
+            return "You won!"
+        } else {
+            return "You scored 5 points, you are winner!"
+        }  
 }
 
 function computerPlay() {
